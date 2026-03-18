a0408900a9a3ee904ff374f0ea5175de85de782a
JiHyuk Jang <wwh3218@gmail.com>
2026-03-18 19:27:36 +0900

    feat: Franchise.service & Controller @Getmapping 생성 | Franchise Entity Upadate 메서드 추가

---
+++ src/main/java/com/hnt/hntapp/domain/franchise/controller/FranchiseController.java
@@ -0,0 +1,4 @@
+package com.hnt.hntapp.domain.franchise.controller;
+
+public class FranchiseController {
+}
---
+++ src/main/java/com/hnt/hntapp/domain/franchise/service/FranchiseService.java
@@ -0,0 +1,4 @@
+package com.hnt.hntapp.domain.franchise.service;
+
+public class FranchiseService {
+}
